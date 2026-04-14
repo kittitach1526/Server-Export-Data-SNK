b62bb7fcd21bc804dc88a7987e2c7ae64bd3c481
unknown <fanta.kittitach@gmail.com>
2026-04-14 21:20:44 +0700

    update

--- services/pressure.py
+++ services/pressure.py
@@ -60,7 +60,7 @@ def fetch_pressure_today(condition: str, limit=100):
     
     # เปลี่ยน NaN เป็น None เพื่อให้ API ส่งค่า null ได้ถูกต้อง
     # clean_data = df.where(pd.notnull(df), None).to_dict(orient='records')
-    clean_data = df.replace({np.nan: None}).to_dict(orient='records')
+    clean_data = df.replace({np.nan: 0}).to_dict(orient='records')
 
     return clean_data, final_column_order
     
@@ -114,7 +114,7 @@ def fetch_pressure_weekly(condition: str, limit=500):
     # 6. Reindex และแปลงเป็น List of Dict
     df = df.reindex(columns=final_column_order)
     # clean_data = df.where(pd.notnull(df), None).to_dict(orient='records')
-    clean_data = df.replace({np.nan: None}).to_dict(orient='records')
+    clean_data = df.replace({np.nan: 0}).to_dict(orient='records')
 
     return clean_data, final_column_order
 
@@ -168,6 +168,6 @@ def fetch_pressure_monthly(condition: str, limit=1000):
     # 6. Reindex และจัดการค่าว่าง (NaN -> None)
     df = df.reindex(columns=final_column_order)
     # clean_data = df.where(pd.notnull(df), None).to_dict(orient='records')
-    clean_data = df.replace({np.nan: None}).to_dict(orient='records')
+    clean_data = df.replace({np.nan:0}).to_dict(orient='records')
 
     return clean_data, final_column_order
